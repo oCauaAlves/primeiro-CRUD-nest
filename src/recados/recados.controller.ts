14aea714f5704cf9fc6ebf7269c19df07ffbf22e
Cauã Alves <caua.rodriguesalves2004@gmail.com>
2026-06-24 11:44:46 -0300

    feat: terminando o controller

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,14 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  @HttpCode(HttpStatus.OK)
   @Get()
-  findAll() {
-    return 'essa rota retorna todos os recados ';
+  findAll(@Query() pagination: { limit?: string; offset?: string }) {
+    const { limit = 10, offset = 0 } = pagination;
+    return `essa rota retorna todos os recados. Limiti=${limit}, offset=${offset} .`;
   }
 
   @Get(':id')
-  findOne() {
-    return 'essa rota retorna UM recado';
+  findOne(@Param('id') id: string) {
+    console.log(id);
+    return `essa rota retorna o recado ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: string) {
+    return body;
+  }
+
+  // o @Patch() -> atualiza apenas algumas partes do recurso
+  // ja o @Put atualiza o recurso inteiro
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: string) {
+    return { id, body };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `ID ${id} deletado com sucesso!`;
   }
 }
