0fc5bd1fa5245abf0ef5b8ea7e3601f8f773701b
Cauã Alves <caua.rodriguesalves2004@gmail.com>
2026-06-24 21:42:15 -0300

    Adicionando segurança com whitelist e forbidNonWhitelisted

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // remove chaves que não estão no DTO
+      forbidNonWhitelisted: true, // levantar erro quando a cheve não existir
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
